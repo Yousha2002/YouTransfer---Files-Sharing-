ed5601f4f98d01f6acbc0e6eecf707e37b27ac23
Yousha2002 <142665211+Yousha2002@users.noreply.github.com>
2025-12-19 21:23:39 +0500

    Your commit message — e.g. added new features & fixed bugs

--- backend/src/routes/transfers.js
+++ backend/src/routes/transfers.js
@@ -1,15 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const transferController = require('../controllers/transferController');
-const auth = require('../middlewares/auth');
-const upload = require('../middlewares/upload');
+const transferController = require("../controllers/transferController");
+const auth = require("../middlewares/auth");
+const upload = require("../middlewares/upload");
 
-router.post('/create', auth, upload.array('files'), transferController.createTransfer);
-router.get('/my-transfers', auth, transferController.getUserTransfers);
-router.get('/:link', transferController.getTransferByLink);
-router.get('/download/:fileId', transferController.downloadFile);
-router.delete('/:transferId', auth, transferController.deleteTransfer);
-router.patch('/deactivate/:transferId', auth, transferController.deactivateTransfer); 
-router.post('/bulk-delete', auth, transferController.bulkDeleteTransfers);
-router.get('/download-all/:transferId', transferController.downloadAllFiles);
+router.post(
+  "/create",
+  auth,
+  upload.array("files"),
+  transferController.createTransfer
+);
+router.get("/my-transfers", auth, transferController.getUserTransfers);
+router.get("/:link", transferController.getTransferByLink);
+router.get("/download/:fileId", transferController.downloadFile);
+router.delete("/:transferId", auth, transferController.deleteTransfer);
+router.patch(
+  "/deactivate/:transferId",
+  auth,
+  transferController.deactivateTransfer
+);
+router.post("/bulk-delete", auth, transferController.bulkDeleteTransfers);
+// router.get("/download-all/:transferId", transferController.downloadAllFiles);
 module.exports = router;
